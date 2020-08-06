965b6546438f44056ab9917c2c11621095bc344e
Cristian <cristian@Criss-MBP.attlocal.net>
2020-08-06 01:37:40 -0700

    upadtes

---
+++ build/precache-manifest.6b469cccfea7055856c841ac9197f4c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533825b608a708bedbe18c4e14381f57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76a3cefee4a150aa4e6c",
+    "url": "/static/css/main.fef564d2.chunk.css"
+  },
+  {
+    "revision": "3358f91f1268d83863bb",
+    "url": "/static/js/2.9edc0cc8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9edc0cc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a3cefee4a150aa4e6c",
+    "url": "/static/js/main.bf806a35.chunk.js"
+  },
+  {
+    "revision": "402f05c59b4e2a57dec5",
+    "url": "/static/js/runtime-main.d1310a82.js"
+  }
+]);
